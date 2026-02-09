7acc4c2c22c8589f04622e697b71616baea65961
Wei Siew <u1529306@utah.edu>
2026-02-09 12:31:43 -0700

    add information on gem5

--- README.md
+++ README.md
@@ -2,3 +2,16 @@
 
 Coming soon: code for submission to ASPLOS 2026.
 
+### gem5 Simulation
+
+The files for gem5 simulation can be found in the gem5 directory. 
+The files are obtained from version 24.1 of gem5.
+
+Requirements:
+gcc version >= 10
+Clang 7 to 16
+SCons 3.0 or greater
+Python 3.6+
+protobuf 2.1+
+
+
---
+++ setup-gem5.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+sudo apt install build-essential scons python3-dev git pre-commit zlib1g zlib1g-dev \
+    libprotobuf-dev protobuf-compiler libprotoc-dev libgoogle-perftools-dev \
+    libboost-all-dev  libhdf5-serial-dev python3-pydot python3-venv python3-tk mypy \
+    m4 libcapstone-dev libpng-dev libelf-dev pkg-config wget cmake doxygen clang-format
+
